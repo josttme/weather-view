f60042bb681447730b384858ccfca9d9b8429331
Josue Tapia <josue.tapia@live.com.ar>
2023-09-14 23:54:04 -0300

    :fire: [REMOVE] API cache JSON files and adjust src/services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 const API_URL = (cityName) =>
 	`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=en&format=json`
-/* const API_URL_2 = '/src/services/api.json' */
 const API_BASE = (lat, long) =>
 	`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&current_weather=true&timeformat=unixtime&timezone=auto`
-/* const API_BASE_2 = './src/services/apiWeather.json' */
 
 function mappedCities(city) {
 	return {
@@ -50,32 +48,9 @@ async function fetchCity({ searchTerm }) {
 		console.error('Error fetching data:', error)
 		throw error
 	}
-	/* 	try {
-		const res = await fetch(API_URL_2)
-		if (!res.ok) {
-			throw new Error('No se pudo cargar el archivo JSON')
-		}
-
-		const data = await res.json()
-		const cities = data?.results?.map((city) => mappedCities(city))
-		return { cities }
-	} catch (error) {
-		console.error('Error fetching and mapping data:', error)
-	} */
 }
 
 async function getWeatherCityByLatLong(latitude, longitude) {
-	/* 	try {
-		const res = await fetch(API_BASE_2)
-		if (!res.ok) {
-			throw new Error('No se pudo cargar el archivo JSON')
-		}
-
-		const data = await res.json()
-		return { data, city, country }
-	} catch (error) {
-		console.error('Error fetching and mapping data:', error)
-	} */
 	const res = await fetch(API_BASE(latitude, longitude))
 	const data = await res.json()
 	return { data }
